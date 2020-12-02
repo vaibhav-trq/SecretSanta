df315aa6306071eb4383efd20b9337cf24a7a74f
Vaibhav Gupta <vaibhav@therightquestion.dev>
2020-12-01 22:47:59 -0800

    Adding typescript and pug to html

---
+++ public/src/js/main.ts
@@ -0,0 +1,40 @@
+const { firebase } = window;
+
+document.addEventListener('DOMContentLoaded', function () {
+  const loadEl = document.querySelector('#load')!;
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.firestore().doc('/foo/bar').get().then(() => { });
+  // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  // firebase.analytics(); // call to activate
+  // firebase.analytics().logEvent('tutorial_completed');
+  // firebase.performance(); // call to activate
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  try {
+    const app = firebase.app();
+    const features = [
+      'auth',
+      'database',
+      'firestore',
+      'functions',
+      'messaging',
+      'storage',
+      'analytics',
+      'remoteConfig',
+      'performance',
+    ].filter((feature: string) => {
+      // @ts-ignore: TS7053
+      return typeof app[feature] === 'function';
+    });
+    loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
+  } catch (e) {
+    console.error(e);
+    loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+  }
+});
