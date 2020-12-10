a4e3c5731517eb556cfb61f26c3d8232e9fba428
Vaibhav Gupta <vaibhavtheory@gmail.com>
2020-12-09 18:35:23 -0800

    Adding better emulator debugging

--- emulate.sh
+++ emulate.sh
@@ -14,4 +14,16 @@ npm --prefix functions run build
 # Start emulator.-
 GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/google_creds.json
 FIREBASE_DATABASE_EMULATOR_HOST="localhost:9000"
-firebase emulators:start --only hosting,database,functions,auth --import=./data --export-on-exit
+firebase emulators:start --only hosting,database,functions,auth --import=./data --export-on-exit &
+EMULATOR_PID=$!
+
+# Start watchers in the background
+npm --prefix public run watch_ts &
+HOSTING_TS_PID=$!
+
+npm --prefix public run watch_pug &
+HOSTING_PUG_PID=$!
+
+read  -n 1 -p "Press Enter to Stop:" mainmenuinput
+
+trap 'kill $(jobs -p) && wait && echo Successfully exited' SIGINT SIGTERM EXIT
